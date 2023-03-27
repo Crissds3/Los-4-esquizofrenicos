f51c8c5a9577fdc831d9634bbed8eadaaedfc5f6
Cristóbal Nicolás Núñez Medina <cnunez21@alumnos.utalca.cl>
2023-03-26 23:48:15 -0300

    Creacion de las cajas y los numeros mediante un label

--- Los 4 esquizofrenicos/src/los/pkg4/esquizofrenicos/FXMLInterfazController.java
+++ Los 4 esquizofrenicos/src/los/pkg4/esquizofrenicos/FXMLInterfazController.java
@@ -9,7 +9,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class FXMLInterfazController implements Initializable {
 
@@ -18,11 +23,39 @@ public class FXMLInterfazController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Rectangulo[] r = new Rectangulo[16];
+        Rectangle[] rec = new Rectangle[16];
+        Label[] numeros = new Label[16]; 
+        StackPane paneLabel;
+        //Crea los label
+        for (int i = 0; i < 16; i++) {
+            numeros[i] = new Label();
+            numeros[i].setTranslateY(700);
+            numeros[i].setTranslateX(360+i*80);
+            numeros[i].setLayoutX(50);
+            numeros[i].setLayoutY(50);
+            numeros[i].setTextFill(Color.WHITE);
+            numeros[i].setStyle("-fx-font-weight: bold");   
+        }
+        paneLabel = new StackPane(numeros);
+        Group root2 = new Group(paneLabel);
+        // Crea cuadrados
+        for (int i = 0; i < 16; i++) {
+           int numero = (int)(Math.random()*(99+1));
+            r[i] = new Rectangulo(numero,60, 60, Color.GREEN);
+            r[i].r.setTranslateY(676);
+            r[i].r.setTranslateX(340+i*80);
+            rec[i] = new Rectangle();
+            rec[i] = r[i].r;
+            numeros[i].setText(numero+"");
+        }    
+        
+        StackPane pane = new StackPane(rec);
+        Group root = new Group(pane);
+        myAnchorPane.getChildren().add(root);
+        myAnchorPane.getChildren().add(root2);
     }    
  
-   
-    
     void sort(int arr[]){
         int n = arr.length;
         for (int i = 1; i < n; i++) {
@@ -35,6 +68,4 @@ public class FXMLInterfazController implements Initializable {
             arr[j + 1] = key;
         }
     }
-
-    
 }
