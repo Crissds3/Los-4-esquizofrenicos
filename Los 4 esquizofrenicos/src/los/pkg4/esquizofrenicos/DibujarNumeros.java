495332eae6413a944d75c9745f465dd1019778cf
Pablo Correa <pcorrea21@alumnos.utalca.cl>
2023-04-02 20:28:34 -0400

    Correcion de errores de compatibilidad

--- Los 4 esquizofrenicos/src/los/pkg4/esquizofrenicos/DibujarNumeros.java
+++ Los 4 esquizofrenicos/src/los/pkg4/esquizofrenicos/DibujarNumeros.java
@@ -5,61 +5,46 @@ import javafx.scene.paint.Color;
 
 public class DibujarNumeros {
 
-    /*
-    recibe el graphicsContext y con eso le paso las coordenadas para dibujar.
-    por ej se invoca la funcion 0, con las x e y donde quiero que se dibujen para luego utilizar ese arreglo que retorna
-    la funcion y utilizar el graphicsContext
-    
-    -Se llama la funcion dibujar numero que recibe graphicsContext
-    -Se invoca una funcion de la clase numero
-    -Se utiliza el arreglo de la funcion anterior y mediante un for se dibuja el numero
-     */
-    protected void dibujar(GraphicsContext gc) {
-        Numeros cero = new Numeros();
-        double[] coordenadaCero = cero.cero(100, 100);
-
-        for (int i = 0; i < coordenadaCero.length; i = i + 4) {
-            gc.setStroke(Color.RED);
-            gc.strokeLine(coordenadaCero[i], coordenadaCero[i + 1], coordenadaCero[i + 2], coordenadaCero[i + 3]);
+    protected void dibujar(GraphicsContext gc, int num, int x, int y) {
+        Numeros numDibujar = new Numeros();
+        double[] coordenada = null;
+        switch(num){
+                    case 0:
+                coordenada = numDibujar.cero(x, y);
+                break;
+            case 1:
+                coordenada = numDibujar.uno(x, y);
+                break;
+            case 2:
+                coordenada = numDibujar.dos(x, y);
+                break;
+            case 3:
+                coordenada = numDibujar.tres(x, y);
+                break;
+            case 4:
+                coordenada = numDibujar.cuatro(x, y);
+                break;
+            case 5:
+                coordenada = numDibujar.cinco(x, y);
+                break;
+            case 6:
+                coordenada = numDibujar.seis(x, y);
+                break;
+            case 7:
+                coordenada = numDibujar.siete(x, y);
+                break;
+            case 8:
+                coordenada = numDibujar.ocho(x, y);
+                break;
+            case 9:
+                coordenada = numDibujar.nueve(x, y);
+                break;
+            default:
+                break;
         }
-
-        Numeros uno = new Numeros();
-        double[] coordenadaUno = uno.uno(150, 100);
-
-        for (int i = 0; i < coordenadaUno.length; i = i + 4) {
-            gc.setStroke(Color.RED);
-            gc.strokeLine(coordenadaUno[i], coordenadaUno[i + 1], coordenadaUno[i + 2], coordenadaUno[i + 3]);
+        for (int i = 0; i < coordenada.length; i = i + 4) {
+            gc.setStroke(Color.WHITE);
+            gc.strokeLine(coordenada[i], coordenada[i + 1], coordenada[i + 2], coordenada[i + 3]);
         }
-
-        Numeros dos = new Numeros();
-        double[] coordenadaDos = dos.dos(200, 100);
-
-        for (int i = 0; i < coordenadaDos.length; i = i + 4) {
-            gc.setStroke(Color.RED);
-            gc.strokeLine(coordenadaDos[i], coordenadaDos[i + 1], coordenadaDos[i + 2], coordenadaDos[i + 3]);
-        }
-
-        Numeros tres = new Numeros();
-        double[] coordenadaTres = tres.tres(250, 100);
-
-        for (int i = 0; i < coordenadaTres.length; i = i + 4) {
-            gc.setStroke(Color.RED);
-            gc.strokeLine(coordenadaTres[i], coordenadaTres[i + 1], coordenadaTres[i + 2], coordenadaTres[i + 3]);
-        }
-        Numeros cuatro = new Numeros();
-        double[] coordenadaCuatro = cuatro.cuatro(300, 100);
-
-        for (int i = 0; i < coordenadaCuatro.length; i = i + 4) {
-            gc.setStroke(Color.RED);
-            gc.strokeLine(coordenadaCuatro[i], coordenadaCuatro[i + 1], coordenadaCuatro[i + 2], coordenadaCuatro[i + 3]);
-        }
-        Numeros cinco = new Numeros();
-        double[] coordenadaCinco = cinco.cinco(350, 100);
-
-        for (int i = 0; i < coordenadaCinco.length; i = i + 4) {
-            gc.setStroke(Color.RED);
-            gc.strokeLine(coordenadaCinco[i], coordenadaCinco[i + 1], coordenadaCinco[i + 2], coordenadaCinco[i + 3]);
-        }
-
     }
 }
