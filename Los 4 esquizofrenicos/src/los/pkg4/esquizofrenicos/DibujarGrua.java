8c3ed097eca65153962ea9460b0bc9eb3c6d00ba
Yanko Acuña <yacuna20@alumnos.utalca.cl>
2023-04-09 18:55:05 -0400

    Animacion de gruas durante los swaps y ordenamiento del codigo

--- Los 4 esquizofrenicos/src/los/pkg4/esquizofrenicos/DibujarGrua.java
+++ Los 4 esquizofrenicos/src/los/pkg4/esquizofrenicos/DibujarGrua.java
@@ -5,27 +5,25 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
 public class DibujarGrua {
-  Line cuerda= new Line();
+    Line cuerda= new Line();
     
-    public void dibujarCuerda(int x,int y){
-        
-          cuerda.setStartX(x);
-          cuerda.setStartY(y);
-          cuerda.setEndX(x);
-          cuerda.setEndY(y+95);
-          cuerda.setStrokeWidth(4);
-               
+    public void dibujarCuerda(int x,int y){    
+        cuerda.setStartX(x);
+        cuerda.setStartY(y);
+        cuerda.setEndX(x);
+        cuerda.setEndY(y+95);
+        cuerda.setStrokeWidth(4);        
     }
     
     
     
-      protected void dibujarBase(GraphicsContext dib, int x, int y){
+    protected void dibujarBase(GraphicsContext dib, int x, int y){
 
         double[] coordenadaBase = {
-            x,y ,x+100,y,
+            x,y ,x+70,y,
             x,y,x,y+50,
-            x,y+50,x+100,y+50,
-            x+100,y+50,x+100,y 
+            x,y+50,x+70,y+50,
+            x+70,y+50,x+70,y 
         };
         for (int i = 0; i < coordenadaBase.length; i = i + 4) {
             dib.setStroke(Color.BLACK);
@@ -35,8 +33,8 @@ public class DibujarGrua {
     }
       
       
-      protected void dibujarIman(GraphicsContext dib, int x, int y){
-           double[] coordenadaIman = {
+    protected void dibujarIman(GraphicsContext dib, int x, int y){
+        double[] coordenadaIman = {
             x-20,y,x+20,y,
             x-20,y,x-30,y+30,
             x-30,y+30,x-30,y+60,
